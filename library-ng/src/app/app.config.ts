fb29edb8494cb4f274f179864ec5706ede419c29
Brian McSwiggen <brian.mcswiggen@gmail.com>
2024-10-29 15:34:53 -0400

    Add firebase to angular project

--- library-ng/src/app/app.config.ts
+++ library-ng/src/app/app.config.ts
@@ -3,7 +3,25 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideFirebaseApp(() => initializeApp(
+        {
+          "projectId":"run-web",
+          "appId":"1:869102415447:web:2773f423e767419dfbbd12",
+          "storageBucket":"run-web.appspot.com",
+          "locationId":"us-east4",
+          "apiKey":"AIzaSyAA21NTAgAct2HZJj-Kd-OROGZXlkSqVd4",
+          "authDomain":"run-web.firebaseapp.com",
+          "messagingSenderId":"869102415447",
+          "measurementId":"G-H3YTWYL0DY"
+        })),
+    provideAuth(() => getAuth()),
+  ]
 };
