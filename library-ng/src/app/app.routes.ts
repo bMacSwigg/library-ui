774182384c090edf14cee175847ddac3469025d9
Brian McSwiggen <brian.mcswiggen@gmail.com>
2024-11-19 13:34:29 -0500

    Add new components & routing

--- library-ng/src/app/app.routes.ts
+++ library-ng/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LandingComponent } from './landing/landing.component';
+import { AccountDetailsComponent } from './account-details/account-details.component';
 import { MyLibraryComponent } from './my-library/my-library.component';
+import { UsersListComponent } from './users-list/users-list.component';
 import { authGuard } from './auth.guard';
 
 export const routes: Routes = [
@@ -13,6 +15,18 @@ export const routes: Routes = [
     path: 'my-library',
     component: MyLibraryComponent,
     title: 'My library',
+    canActivate: [authGuard],
+  },
+  {
+    path: 'account',
+    component: AccountDetailsComponent,
+    title: 'My account',
+    canActivate: [authGuard],
+  },
+  {
+    path: 'users',
+    component: UsersListComponent,
+    title: 'Users',
     canActivate: [authGuard],
   },
 ];
